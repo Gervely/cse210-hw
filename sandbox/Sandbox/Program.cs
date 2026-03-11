d75085475875a9571c1c7dfb11851996eb192b67
Gervely <gbazi@example.com>
2026-03-11 18:49:21 +0100

    Made adjustments to the code.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!! yes this is my Sandbox Project.");
+        Console.WriteLine("Hello Sandbox World! yes this is my Sandbox Project.");
     }
 }
