7201fa2afbfcf068488643e1f865e826e4f56fef
Gervely <gbazi@example.com>
2026-03-19 16:36:15 +0100

    Changed the display message

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+
+        Random randomGenerator = new Random();
+        int magicNumbers = randomGenerator.Next(1, 101);
+
+         int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
