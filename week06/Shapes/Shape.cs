7f5362fcd9d067a9d0137864f2a4a6f7f6e8e83e
Gervely <gbazi@example.com>
2026-04-13 16:10:49 +0100

    Changed the display message

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,22 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+     public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // Virtual/abstract method for area
+    public abstract double GetArea();
+}
